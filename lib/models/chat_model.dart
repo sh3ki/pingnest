aad673b062ab44d28985d8c18bc2678e9ec966d2
sh3ki <shekaigarcia@gmail.com>
2026-03-08 18:09:24 +0800

    Create lib/models/chat_model.dart

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,69 @@
+class Contact {
+  final String id;
+  final String name;
+  final String avatar; // emoji
+  final bool isOnline;
+  final String status;
+  final String phone;
+
+  const Contact({
+    required this.id,
+    required this.name,
+    required this.avatar,
+    this.isOnline = false,
+    this.status = '',
+    this.phone = '',
+  });
+}
+
+enum MessageType { text, image, audio, emoji }
+
+class Message {
+  final String id;
+  final String senderId;
+  final String text;
+  final MessageType type;
+  final DateTime timestamp;
+  bool isRead;
+
+  Message({
+    required this.id,
+    required this.senderId,
+    required this.text,
+    this.type = MessageType.text,
+    required this.timestamp,
+    this.isRead = true,
+  });
+}
+
+class Conversation {
+  final String id;
+  final Contact contact;
+  final List<Message> messages;
+  int unreadCount;
+
+  Conversation({
+    required this.id,
+    required this.contact,
+    required this.messages,
+    this.unreadCount = 0,
+  });
+
+  Message? get lastMessage => messages.isNotEmpty ? messages.last : null;
+}
+
+class Story {
+  final Contact contact;
+  final String emoji;
+  final String caption;
+  final DateTime timestamp;
+  bool viewed;
+
+  Story({
+    required this.contact,
+    required this.emoji,
+    required this.caption,
+    required this.timestamp,
+    this.viewed = false,
+  });
+}
